83f089b4886ec0e3674d9629695e6f64e9f67569
Joradn <jrdnlake@gmail.com>
2020-06-03 17:13:52 +0100

    base code for webhost based off json test

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Spin the Dridel - [Basic Azure Function](https://github.com/microsoft/25-days-of
 
 Install the azure function in VS Code, installed 2.2 version of dotnet core
 
+?name=jordan
+
 Run in web browser in local dev [http://localhost:7071/api/Dridel] this calls the code in the run.ps1
 
 Publish to via VS Code to Azure [here](https://jl-dreidel.azurewebsites.net/api/Dreidel)
--- day3/GithubUpload/run.ps1
+++ day3/GithubUpload/run.ps1
@@ -3,23 +3,31 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+
+#Github URL
+
+$githubUrl = "https://github.com/lake28/25DaysOfServerless/blob/master/day3/images/"
+
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
+# Got site to test webhooks -- https://webhook.site/
+$imagesAdded = $Request.Body.commits.added | Where-Object $_ -Like "*.png"
 
-if ($name) {
-    $status = [HttpStatusCode]::OK
-    $body = "Hello $name"
-}
-else {
-    $status = [HttpStatusCode]::BadRequest
-    $body = "Please pass a name on the query string or in the request body."
-}
+Write-Host " Added images: $imagesAdded"
+
+### create a url for image ?
+
+$url = "https://github.com/lake28/25DaysOfServerless/blob/master/day3/images/" + $imagesAdded
+
+
+
+
+
+#### save  data to a database, table, queue?
+
+
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
